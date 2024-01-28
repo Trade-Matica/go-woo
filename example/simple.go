ec37796198f62f8ee816e91a09855666acab495d
pk <pk@codeblob.work>
2024-01-29 00:20:17 +0100

    Add example for order sending

--- example/simple.go
+++ example/simple.go
@@ -3,17 +3,27 @@ package main
 import (
 	"fmt"
 
-	gowoo "github.com/trading-peter/go-woo/go-woo"
+	v1 "github.com/trading-peter/go-woo/v1"
+	v1Types "github.com/trading-peter/go-woo/v1/types"
 )
 
 func main() {
-	client := gowoo.NewClient(gowoo.WithAuth("xxx", "xxx"))
+	client := v1.NewClient(v1.WithAuth("your api key", "your api secret"))
 
 	// Get client info
-	clientInfo, err := client.GetAccountInfoV3()
-	if err != nil {
-		fmt.Println("Error getting client info:", err)
-	} else {
-		fmt.Println("Client Info:", string(clientInfo))
+	symbols, _ := client.AvailableSymbols()
+
+	for _, asr := range symbols.Rows {
+		fmt.Printf("%+v\n", asr.Symbol)
 	}
+
+	orderResult, err := client.SendOrder(v1Types.Order{
+		Symbol:        "PERP_SOL_USDT",
+		OrderType:     v1Types.LIMIT,
+		OrderPrice:    90.5,
+		OrderQuantity: 1,
+		Side:          v1Types.BUY,
+	})
+	fmt.Printf("%+v\n", orderResult)
+	fmt.Printf("%+v\n", err)
 }
